36974dffab167c8db1def782b8d71f04b0a4998b
suhayb@imagine.com.jo <suhayb@imagine.com.jo>
2017-11-12 16:18:11 +0200

    intial setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # imagination
 Starter Kit for Front-End application include setup for Webpack, React, PaperJS and many more.
+
+Use it to start developing Front-End applications.
+
+### Running Locally (Development Mode)
+```sh
+git clone https://github.com/suhaybimagine/imagination.git
+npm install
+npm run dev
+```
+
+### Production
+
+Use the following command to bundle the application:
+
+```sh
+npm run build
+```
---
+++ src/Main.js
@@ -0,0 +1,44 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Paper from "paper";
+import TWEEN from "tween.js";
+import styles from "./styles/box.css";
+
+class Root extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <h1>Hello World</h1>
+                <div className={styles.box}></div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Root />, document.getElementById("app"));
+
+Paper.install(window);
+window.onload = function () {
+
+    Paper.setup('main-board');
+    Paper.view.on("frame", function (e) {
+        TWEEN.update();
+    });
+
+    initCanvas();
+}
+
+function initCanvas() {
+
+    console.log("PaperJS canvas is up and running !.");
+
+    let stick = new Paper.Path.Rectangle(new Rectangle(0, 0, 100, 4));
+    stick.fillColor = "red";
+    stick.position = new Point(100, 300);
+
+    stick.on("frame", function(e){
+        this.rotation += 3;
+    });
+}
